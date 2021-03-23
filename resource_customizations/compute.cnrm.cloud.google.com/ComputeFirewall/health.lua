b09ebb47e3e96cda878cf65290eea9485322933a
Brian Peck <brian.peck@radpartners.com>
2021-03-23 12:41:07 -0700

    add missing unknown and degraded conditions

--- resource_customizations/compute.cnrm.cloud.google.com/ComputeFirewall/health.lua
+++ resource_customizations/compute.cnrm.cloud.google.com/ComputeFirewall/health.lua
@@ -2,12 +2,27 @@ hs = {}
 if obj.status ~= nil then
   if obj.status.conditions ~= nil then
     for i, condition in ipairs(obj.status.conditions) do
-      if condition.type == "Ready" and condition.status == "False" then
+      if condition.type == "Ready" and condition.status == "False" and condition.reason == "DependencyInvalid" then
+        hs.status = "Degraded"
+        hs.message = condition.message
+        return hs
+      end
+      if condition.type == "Ready" and condition.status == "False" and condition.reason == "DependencyNotFound" then
+        hs.status = "Unknown"
+        hs.message = condition.message
+        return hs
+      end
+      if condition.type == "Ready" and condition.status == "False" and condition.reason == "DependencyNotReady" then
+        hs.status = "Missing"
+        hs.message = condition.message
+        return hs
+      end
+      if condition.type == "Ready" and condition.status == "False" and condition.reason == "Updating" then
         hs.status = "Progressing"
         hs.message = condition.message
         return hs
       end
-      if condition.type == "Ready" and condition.status == "True" then
+      if condition.type == "Ready" and condition.status == "True" and condition.reason == "UpToDate" then
         hs.status = "Healthy"
         hs.message = condition.message
         return hs
