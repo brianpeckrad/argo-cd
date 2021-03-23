d9257385dd1b6471bdbf2894359b16f697d55323
Brian Peck <brian.peck@radpartners.com>
2021-03-23 12:15:18 -0700

    Add unknown dependency not found status

--- resource_customizations/dns.cnrm.cloud.google.com/DNSRecordSet/health.lua
+++ resource_customizations/dns.cnrm.cloud.google.com/DNSRecordSet/health.lua
@@ -2,6 +2,11 @@ hs = {}
 if obj.status ~= nil then
   if obj.status.conditions ~= nil then
     for i, condition in ipairs(obj.status.conditions) do
+      if condition.type == "Ready" and condition.status == "False" and condition.reason == "DependencyNotFound" then
+        hs.status = "Unknown"
+        hs.message = condition.message
+        return hs
+      end
       if condition.type == "Ready" and condition.status == "False" and condition.reason == "DependencyNotReady" then
         hs.status = "Missing"
         hs.message = condition.message
