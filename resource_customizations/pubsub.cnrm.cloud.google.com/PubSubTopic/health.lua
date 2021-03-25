a303f371c2c4ab7f72f215f3cbb73116655c9ffd
Brian Peck <brian.peck@radpartners.com>
2021-03-25 09:00:38 -0700

    add missing, degraded, unknown test for PubSubTopic

--- resource_customizations/pubsub.cnrm.cloud.google.com/PubSubTopic/health.lua
+++ resource_customizations/pubsub.cnrm.cloud.google.com/PubSubTopic/health.lua
@@ -2,21 +2,6 @@ hs = {}
 if obj.status ~= nil then
   if obj.status.conditions ~= nil then
     for i, condition in ipairs(obj.status.conditions) do
-      if condition.type == "Ready" and condition.status == "False" and condition.reason == "DependencyInvalid" then
-        hs.status = "Degraded"
-        hs.message = condition.message
-        return hs
-      end
-      if condition.type == "Ready" and condition.status == "False" and condition.reason == "DependencyNotFound" then
-        hs.status = "Unknown"
-        hs.message = condition.message
-        return hs
-      end
-      if condition.type == "Ready" and condition.status == "False" and condition.reason == "DependencyNotReady" then
-        hs.status = "Missing"
-        hs.message = condition.message
-        return hs
-      end
       if condition.type == "Ready" and condition.status == "False" then
         hs.status = "Progressing"
         hs.message = condition.message
